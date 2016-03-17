b3a2b013daebc7f864d3704976182073149c4aff
wangsl <123456>
2016-03-17 18:28:27 +0800

    结构调整，引入angularjs组织页面。

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var singleDiv = angular.module('singleDiv',['ngRoute']);
+singleDiv.config(['$httpProvider','$routeProvider', function ($httpProvider,$routeProvider) {
+    $routeProvider
+        .when('/', {templateUrl:'/tpl/shapes.html'})
+        .when('/create', {templateUrl:'/tpl/create.html'})
+        .when('/works', {templateUrl:'/tpl/works.html'})
+        .otherwise({redirectTo:'/'});
+}]);
