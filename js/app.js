3eabbf32d5b7d72c0fd5b92a20585e0d845c72bf
wangsl <123456>
2016-03-28 13:50:01 +0800

    去掉第三个页面添加一个css3动画。

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 var singleDiv = angular.module('singleDiv',['ngRoute']);
 singleDiv.config(['$httpProvider','$routeProvider', function ($httpProvider,$routeProvider) {
     $routeProvider
-        .when('/', {templateUrl:'tpl/shapes.html'})
-        .when('/create', {templateUrl:'tpl/create.html'})
-        .when('/impress', {templateUrl:'tpl/impress.html'})
+        .when('/', {templateUrl:'tpl/shapes.html',controller:'shapeCtrl'})
+        .when('/create', {templateUrl:'tpl/create.html',controller:'createCtrl'})
+        .when('/impress', {templateUrl:'tpl/impress.html',controller:'impressCtrl'})
         .otherwise({redirectTo:'/'});
+}]);
+
+singleDiv.controller('shapeCtrl',['$scope','$anchorScroll',function ($cope,$anchorScroll) {
+    $anchorScroll(0);
+}])
+    .controller('createCtrl',['$scope','$anchorScroll',function ($cope,$anchorScroll) {
+    $anchorScroll(0);
+}])
+    .controller('impressCtrl',['$scope','$anchorScroll',function ($cope,$anchorScroll) {
+    $anchorScroll(0);
 }]);
